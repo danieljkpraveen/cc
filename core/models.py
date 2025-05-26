29b9488d3b88493f794ff0b5316921cfed4619c4
danieljkpraveen <jkdanielpraveen@proton.me>
2025-05-26 12:56:49 +0000

    wip

---
+++ core/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class UploadExcelModel(models.Model):
+    """
+    Model to store uploaded Excel files.
+    """
+    file = models.FileField(upload_to='static/excel/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Excel File: {self.file.name} uploaded at {self.uploaded_at}"
+
+
+class UploadEMLModel(models.Model):
+    """
+    Model to store uploaded EML files.
+    """
+    file = models.FileField(upload_to='static/eml/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"EML File: {self.file.name} uploaded at {self.uploaded_at}"
